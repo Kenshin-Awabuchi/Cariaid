c15924a143051af046c10b60a1a221b337b3ab95
Kenshin-Awabuchi <s2301008@sendai-nct.jp>
2025-04-19 11:41:21 +0900

    Add files via upload
    Signed-off-by: Kenshin-Awabuchi <s2301008@sendai-nct.jp>

--- script.js
+++ script.js
@@ -54,3 +54,43 @@ dots.forEach((dot, index) => {
     startSlider();
   });
 });
+
+
+
+
+//ログイン、アウト
+/*
+ type="module"
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyB-kZseSpbFLvXYVBy5bmsw-M2jHVorLZ8",
+    authDomain: "cariaid.firebaseapp.com",
+    projectId: "cariaid",
+    storageBucket: "cariaid.appspot.com",
+    messagingSenderId: "388903287463",
+    appId: "1:388903287463:web:60cfe73f87131e47db7982"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  const container = document.getElementById("auth-button-container");
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // ログイン中 → ログアウトボタンを表示
+      container.innerHTML = `<button onclick="logout()">Logout</button>`;
+    } else {
+      // 未ログイン → ログインページへ誘導
+      container.innerHTML = `<a href="loguin_en.html">Login</a>`;
+    }
+  });
+
+  // ログアウト処理
+  window.logout = async function () {
+    await signOut(auth);
+    location.reload(); // 状態を更新
+  };
+*/
